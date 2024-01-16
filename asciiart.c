09f40e6cc5bb96a0fe629497f56a09663f24bba4
GuyPoop <2027relova.anji@fcboe.org>
2024-01-16 14:37:26 -0500

    Made ascii art

---
+++ asciiart.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("      00000\n");
+	printf("     00000000\n");
+	printf("      000000\n");
+	printf("     0	||\n");
+	printf("    000 ||\n");
+	printf("     ||_||\n");
+	printf("     |__||\n");
+	printf("        ||\n");
+	printf("        ||\n");
+	printf("        ||\n");
+	printf("   ----/||----- \n");
+	return 0;
+}
